60fc91f78a6604c33b5cbf3afc05633167f46ec5
Danu Rodion <danurodion@gmail.com>
2023-10-05 20:04:12 +0300

    Update for testing push

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./Navbar.css";
 
 const navbar = () => {
-  return <nav className="nav"></nav>;
+  return <nav className="nav">Navbar</nav>;
 };
 
 export default navbar;
